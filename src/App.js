3ecfb6bff1e0260839f5d3b04d6223986bb25755
fur1ozz <tomassveveris@gmail.com>
2024-07-29 16:45:02 +0300

    setup(react, tailwind, router, darkmode)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="min-h-screen dark:bg-[#1d2125] font-topper">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate to="/home" />}
+                    />
+                    <Route
+                        path="*"
+                        element={<Navigate to="/home" />}
+                    />
+                    <Route exact path="/home" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
