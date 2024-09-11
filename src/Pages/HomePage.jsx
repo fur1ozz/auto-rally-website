2695464655e83d50abf319a5df2660e348196dda
fur1ozz <tomassveveris@gmail.com>
2024-09-12 00:55:49 +0300

    created better version of upcoming event container

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from "../components/UIFoundation/Header";
-import UpcomingEvent from "../components/homePageComponents/UpcomingEvent";
 import Footer from "../components/UIFoundation/Footer";
 import HomeCalendar from "../components/homePageComponents/HomeCalendar";
 import RallyQuotes from "../components/homePageComponents/RallyQuotes";
 import PreviousWinner from "../components/homePageComponents/PreviousWinner";
+import UpcomingEvent from "../components/homePageComponents/UpcomingEvent";
 
 const HomePage = () => {
     return (
--- src/components/homePageComponents/UpcomingEvent.jsx
+++ src/components/homePageComponents/UpcomingEvent.jsx
@@ -1,25 +1,62 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const UpcomingEvent = () => {
     return (
-        <div
-            className="bg-cover bg-center cursor-pointer select-none group"
-            style={{ backgroundImage: 'url(/images/headers/rally-utena-2024.png)' }}
-            // onClick={sendTo}
-            id="event"
-        >
-            <div className="w-full backdrop-blur-md bg-black/70 flex justify-center items-center text-white sm:px-10 px-7 shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]">
-                <div className="w-[1024px] group-hover:scale-[0.98] transition duration-300">
-                    <div className="font-bold md:text-3xl sm:text-2xl text-lg py-4">Upcoming event</div>
-                    <img src="/images/headers/rally-utena-2024.png" alt="" className="w-full shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]"/>
-                    <div className="flex justify-between  font-semibold sm:text-lg text-[10px]  sm:px-6 py-4">
-                        <div className="">Pēc - 20d 14h 30m</div>
-                        <div>20.08.24. - 21.08.24.</div>
+        <Link to="#upcoming-event">
+            <div
+                className="bg-cover bg-center cursor-pointer"
+                style={{ backgroundImage: 'url(/images/parallax-backgrounds/for-home/rally-road-1.png)' }}
+            >
+                <div className="w-full backdrop-blur-sm bg-black/20 flex justify-center items-center text-white sm:px-14 px-4">
+                    <div className="w-[1024px] font-chakra ">
+                        <div className="flex items-center pt-10">
+                            <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5">Upcoming Event</div>
+                            <div className="flex-1 h-0.5 bg-white"></div>
+                        </div>
+                        <h2 className="flex items-center justify-center md:text-3xl sm:text-2xl text-lg font-semibold py-4">Samsonas Rally Utena</h2>
+                        <img src="/images/headers/rally-utena-2024.png" alt="" className="w-full shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]"/>
+                        <div className="flex justify-between font-medium sm:text-lg text-[10px] sm:px-6 py-4">
+                            <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-5 size-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                </svg>
+                                <div className="ml-2">20.08. - 21.08.</div>
+                            </div>
+                            <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-5 size-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                                </svg>
+                                <div className="ml-2">20d 14h 30m</div>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center sm:py-2 pt-6">
+                            <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5">Weather Forecast</div>
+                            <div className="flex-1 h-0.5 bg-white"></div>
+                        </div>
+                        <div className="flex justify-between font-semibold font-poppins sm:px-6 sm:py-6 py-4">
+                            <div className="flex flex-col">
+                                <div className="flex items-center sm:text-2xl text-base">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-6 size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
+                                    <div className="ml-1">Utena, Lithuania</div>
+                                </div>
+                                <div className="sm:ml-7 ml-6 sm:text-sm text-[10px] font-normal">Chance of rain: 16%</div>
+
+                                <div className="sm:text-4xl text-2xl my-8">15° - 27°</div>
+                            </div>
+
+                            <div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+        </Link>
     );
 };
 
