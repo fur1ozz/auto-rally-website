5997dfe641734a7b91c206d37673dabd7d6de8a1
fur1ozz <tomassveveris@gmail.com>
2024-10-17 13:24:29 +0300

    added api request to retirements page

--- src/components/rallyPageComponents/componentsForResults/RetirementContainer.jsx
+++ src/components/rallyPageComponents/componentsForResults/RetirementContainer.jsx
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import TitleWithLine from "../../elements/titleWithLine";
-import Flag from "react-flagkit";
-import retirementData from "../../../data/retirementData.json";
 import ResultsTitleLine from "../../elements/ResultsTitleLine";
 import {TableCrew, TableCrewHeading} from "../../elements/tableItems/TableCrew";
 import {TableNumber} from "../../elements/tableItems/TableNumber";
 import TableFlag from "../../elements/tableItems/TableFlag";
 import Table from "../../elements/tableItems/Table";
 import TableHeading from "../../elements/tableItems/TableHeading";
+import {useParams} from "react-router-dom";
+import useFetchData from "../../../hooks/useFetchData";
+import Loader from "../../elements/Loader";
 
-const RetirementItem = ({ number, nationality, coNationality, driver, coDriver, car, retireReason, finishedStages, isOdd }) => {
+const RetirementItem = ({ number, nationality, coNationality, driver, coDriver, car, driveType, retireReason, finishedStages, isOdd }) => {
     return (
         <div
             className={`flex w-full justify-between py-2 border-b border-gray-300 items-center font-light break-words ${isOdd ? 'bg-[#f9f9f9]' : ''}`}
@@ -17,13 +18,18 @@ const RetirementItem = ({ number, nationality, coNationality, driver, coDriver, 
             <TableNumber number={number} />
             <TableFlag nationality={nationality} coNationality={coNationality} />
             <TableCrew driver={driver} coDriver={coDriver} />
-            <div className="w-[30%]">{car}</div>
+            <div className="w-[30%]">{car} ({driveType})</div>
             <div className="flex flex-col w-[30%] font-medium text-red-600">{retireReason}</div>
             <div className="w-[12%] flex flex-col font-semibold items-center pr-2 text-black">{finishedStages}</div>
         </div>
     );
 };
 const RetirementContainer = () => {
+    const { year, rallyName } = useParams();
+    const url = `http://localhost/api/rally-retirements/${year}/${rallyName}`;
+
+    const { data: retirementData, loading, error } = useFetchData(url);
+
     return (
         <section className="w-full min-h-20 bg-white sm:p-14 p-10 flex justify-center">
             <div className="lg:w-[1024px] overflow-x-auto">
@@ -39,18 +45,22 @@ const RetirementContainer = () => {
                             <div className="flex flex-col w-[29%]">Iemesls</div>
                             <div className="w-[13%] flex justify-center text-center pr-2">Pabegtie posmi</div>
                         </TableHeading>
+                        {loading && <Loader />}
+                        {!loading && error && <div>Error loading data: {error.message}</div>}
+
                         {retirementData.length > 0 ? (
-                            retirementData.map((item, index) => (
+                            retirementData.map((retirement, index) => (
                                 <RetirementItem
                                     key={index}
-                                    number={item.crewNumber}
-                                    nationality={item.nationality}
-                                    coNationality={item.coNationality}
-                                    driver={item.driver}
-                                    coDriver={item.coDriver}
-                                    car={item.vehicle}
-                                    retireReason={item.retireReason}
-                                    finishedStages={item.finishedStages}
+                                    number={retirement.crew_number}
+                                    nationality={retirement.driver.nationality}
+                                    coNationality={retirement.co_driver.nationality}
+                                    driver={`${retirement.driver.name} ${retirement.driver.surname}`}
+                                    coDriver={`${retirement.co_driver.name} ${retirement.co_driver.surname}`}
+                                    car={retirement.car}
+                                    driveType={retirement.drive_type}
+                                    retireReason={retirement.retirement.retirement_reason}
+                                    finishedStages={retirement.retirement.finished_stages}
                                     isOdd={index % 2 !== 0}
                                 />
                             ))                        ) : (
