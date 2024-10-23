64c130d47669c82e531116eecd2eeb44732ea65c
fur1ozz <tomassveveris@gmail.com>
2024-10-24 00:36:48 +0300

    made great version of the sidebar

--- src/components/UIFoundation/Header.jsx
+++ src/components/UIFoundation/Header.jsx
@@ -1,22 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Link, useLocation, useParams} from "react-router-dom";
 import {useCurrentTime} from "../../hooks/useCurrentTime";
 import {formatTime} from "../../utils/formatTime";
-import {findByRole} from "@testing-library/react";
 import LanguageSwitcher from "../elements/LanguageSwitcher";
+import {useTranslation} from "react-i18next";
+import Sidebar from "./Sidebar";
 
 // TODO need to fix
-const LinkButtonPhone = ({ name, currentPath }) => {
-    const path = `/${name}`;
+// const LinkButtonPhone = ({ name, currentPath }) => {
+//     const path = `/${name}`;
+//     const isActive = currentPath === path;
+//
+//     return (
+//         <li>
+//             <Link
+//                 to={path}
+//                 className={`flex items-center ${isActive ? ' underline decoration-pj-400 decoration-dotted' : ''} hover:bg-low-200 justify-center mx-2 transition-all duration-100 ease-in`}
+//             >
+//                 <span className="text-lg uppercase font-medium whitespace-nowrap text-pj-400">{name}</span>
+//             </Link>
+//         </li>
+//     );
+// };
+const LinkButtonPhone = ({ name, path, currentPath}) => {
     const isActive = currentPath === path;
 
     return (
-        <Link
-            to={path}
-            className={`flex items-center ${isActive ? ' underline decoration-pj-400 decoration-dotted' : ''} hover:bg-low-200 justify-center mx-2 transition-all duration-100 ease-in`}
-        >
-            <span className="text-lg uppercase font-medium whitespace-nowrap text-pj-400">{name}</span>
-        </Link>
+        <li className="h-10 flex items-center capitalize text-black text-lg">
+            <Link
+                to={path}
+                className={`flex items-center ${isActive ? 'text-rally-primary' : ''}`}
+            >
+                {name}
+            </Link>
+        </li>
+    );
+};
+const LinkButtonPhoneRallyInfo = ({ name, path, currentPath, tag}) => {
+    return (
+        <li className="h-10 pl-4 flex items-center capitalize text-black text-lg">
+            <Link
+                to={path}
+                className={`flex items-center ${currentPath.includes(`/${tag}`) ? 'text-rally-primary' : ''}`}
+            >
+                {name}
+            </Link>
+        </li>
     );
 };
 
@@ -24,7 +53,8 @@ const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     const location = useLocation();
     const time = useCurrentTime();
-    const {lng} = useParams()
+    const {lng, year, rallyName} = useParams()
+    const { t } = useTranslation();
 
     const handleMobileMenuToggle = () => {
         setMobileMenuOpen(!mobileMenuOpen);
@@ -32,7 +62,7 @@ const Header = () => {
     return (
         <div className="w-full bg-black/60">
             <nav className="py-2 px-10">
-                <div className="flex justify-between items-center mx-auto">
+                <ul className="flex justify-between items-center mx-auto">
                     <div className="flex">
                         <a href="/" className="flex mr-0 sm:mr-20 items-center">
                             <img src="/icons/LRC-1.png" alt="LRC" className="mr-3 w-12" />
@@ -79,22 +109,17 @@ const Header = () => {
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
-
-                    {/*TODO phone header*/}
-                    <div className={`sm:hidden ${mobileMenuOpen ? "translate-x-0" : "translate-x-[200px]"} w-[200px] h-screen bg-interfaceBar-purple fixed top-0 right-0 p-5 transition-all duration-200 ease-in-out z-10`}>
-                        <button
-                            onClick={handleMobileMenuToggle}
-                            className="text-rally-primary"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                        <LinkButtonPhone name="Sezonas" currentPath={location.pathname} />
-                        <LinkButtonPhone name="projects" currentPath={location.pathname} />
-                        <LinkButtonPhone name="contact" currentPath={location.pathname} />
-                    </div>
-                </div>
+                </ul>
+                {/*TODO phone header*/}
+                <Sidebar
+                    mobileMenuOpen={mobileMenuOpen}
+                    handleMobileMenuToggle={handleMobileMenuToggle}
+                    location={location}
+                    lng={lng}
+                    year={year}
+                    rallyName={rallyName}
+                    t={t}
+                />
             </nav>
         </div>
     );
---
+++ src/components/UIFoundation/Sidebar.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import calendarData from '../../data/calendarData.json';
+
+const LinkButtonPhone = ({ name, path, currentPath }) => {
+    const isActive = currentPath === path;
+
+    return (
+        <li className="min-h-10 flex items-center capitalize text-[#2e2e2e] font-medium text-lg">
+            <Link
+                to={path}
+                className={`flex items-center ${isActive ? 'text-rally-accent font-medium' : ''}`}
+            >
+                {name}
+            </Link>
+        </li>
+    );
+};
+
+const LinkButtonPhoneRallyInfo = ({ name, path, currentPath, tag }) => {
+    return (
+        <li className="min-h-10 pl-4 flex items-center capitalize text-[#2e2e2e] text-lg">
+            <Link
+                to={path}
+                className={`flex items-center ${currentPath.includes(`/${tag}`) ? 'text-rally-accent font-medium' : ''}`}
+            >
+                {name}
+            </Link>
+        </li>
+    );
+};
+const LinkButtonPhoneRallies = ({ name, path, lng }) => {
+    return (
+        <li className="min-h-10 pl-4 flex items-center capitalize text-[#2e2e2e] text-lg">
+            <a
+                href={`/${lng}/2024/${path.replace(/\s+/g, '-').toLowerCase()}/news`}
+                className={`flex items-center`}
+            >
+                {name}
+            </a>
+        </li>
+    );
+};
+
+const Sidebar = ({ mobileMenuOpen, handleMobileMenuToggle, location, lng, year, rallyName, t }) => {
+    return (
+        <ul className={`sm:hidden ${mobileMenuOpen ? "translate-x-0" : "translate-x-[250px]"} flex flex-col justify-start w-[250px] h-screen bg-white/55 backdrop-blur fixed top-0 right-0 px-5 py-3 transition-all duration-200 ease-in-out z-10 shadow-[-10px_0_10px_rgba(0,0,0,0.1)]`}>
+            <li className="h-10 flex items-center capitalize text-black">
+                <button onClick={handleMobileMenuToggle}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </li>
+            <LinkButtonPhone name="Home" path={`/${lng}/home`} currentPath={location.pathname} />
+            <LinkButtonPhone name="Calendar" path={`/${lng}/home#calendar`} currentPath={location.pathname} />
+            {year ? (
+                <div>
+                    <li className="mt-5 h-10 flex items-center capitalize text-black text-lg">
+                        <a className="flex items-center font-medium">Rally Info</a>
+                    </li>
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.news')} path={`/${lng}/${year}/${rallyName}/news`} currentPath={location.pathname} tag="news" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.participants')} path={`/${lng}/${year}/${rallyName}/participants`} currentPath={location.pathname} tag="participants" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.documents')} path={`/${lng}/${year}/${rallyName}/documents`} currentPath={location.pathname} tag="documents" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.spectators')} path={`/${lng}/${year}/${rallyName}/spectators`} currentPath={location.pathname} tag="spectators" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.results')} path={`/${lng}/${year}/${rallyName}/results`} currentPath={location.pathname} tag="results" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.championship')} path={`/${lng}/${year}/${rallyName}/championship`} currentPath={location.pathname} tag="championship" />
+                    <LinkButtonPhoneRallyInfo name={t('rally-menu-bar.gallery')} path={`/${lng}/${year}/${rallyName}/gallery`} currentPath={location.pathname} tag="gallery" />
+                </div>
+            ) : (
+                <div>
+                    <li className="mt-5 h-10 flex items-center capitalize text-black text-lg">
+                        <a className="flex items-center font-medium">2024</a>
+                    </li>
+                    {calendarData.map((rally, index) => (
+                        <LinkButtonPhoneRallies
+                            key={index}
+                            name={rally.rally_name}
+                            path={rally.eng_name}
+                            lng={lng}
+                        />
+                    ))}
+                </div>
+            )}
+        </ul>
+    );
+};
+
+export default Sidebar;
