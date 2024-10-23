abc65c3cc4f9214af606d0e000992be8fa57186e
fur1ozz <tomassveveris@gmail.com>
2024-10-23 23:05:10 +0300

    created documents animation using grid

--- src/components/rallyPageComponents/DocumentContainer.jsx
+++ src/components/rallyPageComponents/DocumentContainer.jsx
@@ -8,17 +8,19 @@ import useLanguage from "../../hooks/useLanguage";
 
 const PDFItem = ({ title, link }) => {
     return(
-        <a
-            className="flex items-center mt-1 w-fit group"
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-            </svg>
-            <span className="ml-2 group-hover:text-rally-primary">{title}</span>
-        </a>
+        <li>
+            <a
+                className="flex items-center mt-1 w-fit group"
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                </svg>
+                <span className="ml-2 group-hover:text-rally-primary">{title}</span>
+            </a>
+        </li>
     );
 }
 const FolderItem = ({ number, title, files }) => {
@@ -26,8 +28,8 @@ const FolderItem = ({ number, title, files }) => {
     const formattedNumber = String(number).padStart(2, '0');
 
     return (
-        <div className="flex flex-col mb-5">
-            <div className={`flex w-full p-2 border-b-2 border-gray-300 text-xl items-center font-normal ${isOpen ? 'border-rally-primary' : ''}`}>
+        <div className="flex flex-col mb-5 min-w-[500px]">
+            <li className={`flex w-full p-2 border-b-2 border-gray-300 text-xl items-center font-normal ${isOpen ? 'border-rally-primary' : ''}`}>
                 <button
                     className="flex"
                     onClick={() => setIsOpen(!isOpen)}
@@ -39,13 +41,15 @@ const FolderItem = ({ number, title, files }) => {
 
                     <span className="ml-2">{`${formattedNumber}. ${title}`}</span>
                 </button>
-            </div>
-            <div
-                className={`flex-col pl-10 text-lg transition-all duration-500 ease-in-out overflow-hidden ${isOpen ? '' : 'max-h-0'}`}
-            >                {files.map((file, index) => (
+            </li>
+            <ul className={`grid transition-all duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
+                <div
+                    className={`flex-col pl-10 text-lg overflow-hidden`}
+                >                {files.map((file, index) => (
                     <PDFItem key={index} title={file.name} link={file.link} />
                 ))}
-            </div>
+                </div>
+            </ul>
         </div>
     );
 };
@@ -62,7 +66,7 @@ const DocumentContainer = () => {
         <section className="w-full min-h-20 bg-white sm:p-14 p-10 flex justify-center">
             <div className="lg:w-[1024px] w-full overflow-x-auto">
                 <TitleWithLine title={t('rally-menu-bar.documents')} />
-                <div className="flex flex-col mt-10 w-full text-[#4e4e4e] font-light">
+                <div className="flex flex-col mt-10 w-full text-[#4e4e4e] font-light overflow-x-auto">
                     {loading && <Loader />}
                     {!loading && error && <div>Error loading data: {error.message}</div>}
 
