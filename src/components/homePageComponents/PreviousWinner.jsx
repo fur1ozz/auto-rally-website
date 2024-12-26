1e7bb8eb25fd1b60d2477912411e0a30414a7727
fur1ozz <tomassveveris@gmail.com>
2024-12-26 22:40:36 +0200

    some kind of update based on date and time for next event

--- src/components/homePageComponents/PreviousWinner.jsx
+++ src/components/homePageComponents/PreviousWinner.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+import useFetchData from "../../hooks/useFetchData";
+import useLanguage from "../../hooks/useLanguage";
+import Loader from "../elements/loaders/Loader";
 
 const PreviousWinner = () => {
+    const { lng } = useParams();
+    const { t } = useTranslation();
+    const url = `http://localhost/api/previousWinner`;
+
+    const { data: prevWinner, loading, error } = useFetchData(url);
+
+    useLanguage(lng);
+
     return (
         <section className="w-full bg-white sm:px-14 px-10 pt-10 flex justify-center">
             <div className="lg:w-[1024px] w-full">
@@ -11,18 +24,41 @@ const PreviousWinner = () => {
                 </div>
                 <div className="flex w-full text-[#4e4e4e] md:flex-row flex-col items-center">
                     <div className="flex flex-col md:flex-row w-[300px] md:w-full items-center font-chakra mt-10 mx-2">
-                        <div className="flex md:mr-4 mb-4 md:mb-0 min-w-72">
-                            <img src="/images/rally-cesis-winner.jpg" alt="" className="h-52 w-72 md:w-96 rounded-sm object-cover shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]"/>
-                        </div>
-                        <div className="flex flex-col justify-between min-h-52 w-fit">
-                            <h3 className="text-2xl font-bold">Rallijs Cēsis</h3>
-                            <div className="text-xl font-normal mb-2 md:mb-0">
-                                <div>Tomass Vēveris</div>
-                                <div>Reinis Artimovičs</div>
-                            </div>
-                            <div className="text-lg font-medium"><span className="font-semibold">Car:</span> Mitsubishi Lancer Evo IX</div>
-                            <p className="font-light indent-4">Šīs bija tiešam labas sajūtas, paldies manam sturmanim par idealu sniegumu, bez viņa mēs šeit uz pjedestāļa nestāvētu.</p>
-                        </div>
+                        {loading && <Loader />}
+                        {!loading && error && <div>Error loading data: {error.message}</div>}
+
+                        {!loading && !error && (
+                            prevWinner? (
+                                <>
+                                    <div className="flex md:mr-4 mb-4 md:mb-0 min-w-72">
+                                        <img src="/images/rally-cesis-winner.jpg" alt="" className="h-52 w-80 rounded-sm object-cover shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]"/>
+                                    </div>
+                                    <div className="flex flex-col justify-between min-h-52 w-full">
+                                        <h3 className="text-2xl font-bold">{prevWinner.rally_name}</h3>
+                                        <div className="text-xl font-normal mb-2 md:mb-0">
+                                            <div>{prevWinner.driver}</div>
+                                            <div>{prevWinner.co_driver}</div>
+                                        </div>
+                                        <div className="text-lg font-medium"><span className="font-semibold">Car: </span>{prevWinner.car}</div>
+                                        <p className="font-light indent-4">{prevWinner.feedback}</p>
+                                    </div>
+                                </>
+                            ) : (
+                                <div></div>
+                            )
+                        )}
+                        {/*<div className="flex md:mr-4 mb-4 md:mb-0 min-w-72">*/}
+                        {/*    <img src="/images/rally-cesis-winner.jpg" alt="" className="h-52 w-72 md:w-96 rounded-sm object-cover shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]"/>*/}
+                        {/*</div>*/}
+                        {/*<div className="flex flex-col justify-between min-h-52 w-fit">*/}
+                        {/*    <h3 className="text-2xl font-bold">Rallijs Cēsis</h3>*/}
+                        {/*    <div className="text-xl font-normal mb-2 md:mb-0">*/}
+                        {/*        <div>Tomass Vēveris</div>*/}
+                        {/*        <div>Reinis Artimovičs</div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="text-lg font-medium"><span className="font-semibold">Car:</span> Mitsubishi Lancer Evo IX</div>*/}
+                        {/*    <p className="font-light indent-4">Šīs bija tiešam labas sajūtas, paldies manam sturmanim par idealu sniegumu, bez viņa mēs šeit uz pjedestāļa nestāvētu.</p>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </div>
--- src/components/homePageComponents/UpcomingEvent.jsx
+++ src/components/homePageComponents/UpcomingEvent.jsx
@@ -17,6 +17,9 @@ const UpcomingEvent = () => {
 
     const daysUntilEvent = nextEvent?.date_from ? calculateDaysUntilEvent(nextEvent.date_from) : null;
 
+    // remove later
+    console.log(daysUntilEvent);
+
     let eventMessage;
     if (daysUntilEvent < 0) {
         eventMessage = `${t('home-upcoming-event.happening')}`;
@@ -39,12 +42,12 @@ const UpcomingEvent = () => {
                     <div className="w-full backdrop-blur-sm bg-black/20 flex justify-center items-center text-white sm:px-14 px-4 overflow-hidden">
                         <div className="w-[1024px] font-chakra hover:scale-[1.02] transition-all duration-300">
                             <div className="flex items-center pt-10">
-                                <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5 capitalize">{t('home-upcoming-event.event')}</div>
+                                <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5 capitalize">{nextEvent.all_events_finished ? t('home-upcoming-event.previous') : t('home-upcoming-event.event')}</div>
                                 <div className="flex-1 h-0.5 bg-white"></div>
                             </div>
                             <h2 className="flex items-center justify-center md:text-3xl sm:text-2xl text-lg font-semibold py-4">{nextEvent.name}</h2>
                             <img src={`/images/headers/${nextEvent.tag}-${nextEvent.year}.png`} alt="" className="w-full shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]" />
-                            <div className="flex justify-between font-medium sm:text-lg text-[10px] sm:px-6 py-4">
+                            <div className="flex justify-between font-medium sm:text-lg text-[10px] sm:px-6 py-4 pb-10">
                                 <div className="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-5 size-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
@@ -59,28 +62,28 @@ const UpcomingEvent = () => {
                                 </div>
                             </div>
 
-                            <div className="flex items-center sm:py-2 pt-6">
-                                <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5 capitalize">{t('home-upcoming-event.forecast')}</div>
-                                <div className="flex-1 h-0.5 bg-white"></div>
-                            </div>
-                            <div className="flex justify-between font-semibold font-poppins sm:px-6 sm:py-6 py-4">
-                                <div className="flex flex-col">
-                                    <div className="flex items-center sm:text-2xl text-base">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-6 size-5">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                                        </svg>
-                                        <div className="ml-1">Utena, Lithuania</div>
-                                    </div>
-                                    <div className="sm:ml-7 ml-6 sm:text-sm text-[10px] font-normal">Chance of rain: 16%</div>
+                            {/*<div className="flex items-center sm:py-2 pt-6">*/}
+                            {/*    <div className="font-semibold md:text-3xl sm:text-2xl text-xl mr-5 capitalize">{t('home-upcoming-event.forecast')}</div>*/}
+                            {/*    <div className="flex-1 h-0.5 bg-white"></div>*/}
+                            {/*</div>*/}
+                            {/*<div className="flex justify-between font-semibold font-poppins sm:px-6 sm:py-6 py-4">*/}
+                            {/*    <div className="flex flex-col">*/}
+                            {/*        <div className="flex items-center sm:text-2xl text-base">*/}
+                            {/*            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-6 size-5">*/}
+                            {/*                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />*/}
+                            {/*                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />*/}
+                            {/*            </svg>*/}
+                            {/*            <div className="ml-1">Utena, Lithuania</div>*/}
+                            {/*        </div>*/}
+                            {/*        <div className="sm:ml-7 ml-6 sm:text-sm text-[10px] font-normal">Chance of rain: 16%</div>*/}
 
-                                    <div className="sm:text-4xl text-2xl my-8">15° - 27°</div>
-                                </div>
+                            {/*        <div className="sm:text-4xl text-2xl my-8">15° - 27°</div>*/}
+                            {/*    </div>*/}
 
-                                <div>
+                            {/*    <div>*/}
 
-                                </div>
-                            </div>
+                            {/*    </div>*/}
+                            {/*</div>*/}
                         </div>
                     </div>
                 </div>
