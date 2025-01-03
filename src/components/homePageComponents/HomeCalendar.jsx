9f0731cad2362fff3a19138771ec6d50fdd770d4
fur1ozz <tomassveveris@gmail.com>
2025-01-03 16:38:46 +0200

    Added rally pictures for 2025 calendar

--- src/components/homePageComponents/HomeCalendar.jsx
+++ src/components/homePageComponents/HomeCalendar.jsx
@@ -15,7 +15,7 @@ const CalendarItem = ({ rally_name, date_from, date_to, location, rally_image_fo
             <div className="w-[300px] h-[300px] rounded-md mb-10 mx-2 group cursor-pointer flex flex-col font-chakra shadow-[0_3px_8px_0_rgba(0,0,0,0.17)]">
                 <div className="overflow-hidden h-[55%] flex justify-center items-center rounded-t-md relative">
                     <img
-                        src={`/images/rally-images-for-calendar/${rally_image_for_calendar}`}
+                        src={`/images/rally-images-for-calendar/${year}/${rally_image_for_calendar}`}
                         alt=""
                         className="w-full h-48 object-cover group-hover:scale-110 transition-all duration-200"
                     />
@@ -45,34 +45,68 @@ const CalendarItem = ({ rally_name, date_from, date_to, location, rally_image_fo
 
 const HomeCalendar = () => {
 
-    const year = 2024;
-    const ralliesForYear = calendarData[year] || [];
+    const year2024 = 2024;
+    const rallies_2024 = calendarData[year2024] || [];
+
+    const year2025 = 2025;
+    const rallies_2025 = calendarData[year2025] || [];
 
     return (
-        <section className="w-full min-h-screen bg-white sm:px-14 px-10 pt-10 flex justify-center" id="calendar-section">
-            <div className="lg:w-[1024px]">
-                <div className="flex items-center">
-                    <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
-                    <h2 className="font-containerHeading font-bold text-[#4e4e4e] text-4xl mx-4">2024</h2>
-                    <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
+        <>
+            <section className="w-full min-h-screen bg-white sm:px-14 px-10 pt-10 flex justify-center"
+                     id="calendar-section">
+                <div className="lg:w-[1024px]">
+                    <div className="flex items-center">
+                        <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
+                        <h2 className="font-containerHeading font-bold text-[#4e4e4e] text-4xl mx-4">2024</h2>
+                        <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
+                    </div>
+                    <div
+                        className="flex mt-10 w-full text-[#4e4e4e] justify-between flex-wrap max-[1060px]:justify-evenly">
+                        {rallies_2024.map((rally, index) => (
+                            <CalendarItem
+                                key={index}
+                                rally_name={rally.rally_name}
+                                date_from={rally.date_from}
+                                date_to={rally.date_to}
+                                location={rally.location}
+                                rally_image_for_calendar={rally.rally_image_for_calendar}
+                                eng_name={rally.eng_name}
+                                road_surface={rally.road_surface}
+                                year={year2024}
+                            />
+                        ))}
+                    </div>
                 </div>
-                <div className="flex mt-10 w-full text-[#4e4e4e] justify-between flex-wrap max-[1060px]:justify-evenly">
-                    {ralliesForYear.map((rally, index) => (
-                        <CalendarItem
-                            key={index}
-                            rally_name={rally.rally_name}
-                            date_from={rally.date_from}
-                            date_to={rally.date_to}
-                            location={rally.location}
-                            rally_image_for_calendar={rally.rally_image_for_calendar}
-                            eng_name={rally.eng_name}
-                            road_surface={rally.road_surface}
-                            year={year}
-                        />
-                    ))}
+            </section>
+            <section className="w-full min-h-screen bg-white sm:px-14 px-10 pt-10 flex justify-center"
+                     id="calendar-section">
+                <div className="lg:w-[1024px]">
+                    <div className="flex items-center">
+                        <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
+                        <h2 className="font-containerHeading font-bold text-[#4e4e4e] text-4xl mx-4">2025</h2>
+                        <div className="flex-1 h-0.5 bg-[#4e4e4e]"></div>
+                    </div>
+                    <div
+                        className="flex mt-10 w-full text-[#4e4e4e] justify-between flex-wrap max-[1060px]:justify-evenly">
+                        {rallies_2025.map((rally, index) => (
+                            <CalendarItem
+                                key={index}
+                                rally_name={rally.rally_name}
+                                date_from={rally.date_from}
+                                date_to={rally.date_to}
+                                location={rally.location}
+                                rally_image_for_calendar={rally.rally_image_for_calendar}
+                                eng_name={rally.eng_name}
+                                road_surface={rally.road_surface}
+                                year={year2025}
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
+
     );
 };
 
