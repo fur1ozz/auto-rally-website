d799d01f49170519686e818a707a64bcffe57108
fur1ozz <tomassveveris@gmail.com>
2025-03-27 00:20:11 +0200

    1.04 Delete old news container file and update the new one with dynamic images from backend server

--- src/components/rallyPageComponents/NewsContainer.jsx
+++ src/components/rallyPageComponents/NewsContainer.jsx
@@ -1,61 +1,61 @@
 import React from 'react';
-import TitleWithLine from "../elements/titleWithLine";
+import {useParams} from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData";
+import Loader from "../elements/loaders/Loader";
+import {useTranslation} from "react-i18next";
+import useLanguage from "../../hooks/useLanguage";
 
 const NewsItem = ({ date, title, paragraph, imgSrc }) => {
     return (
         <div className="flex mt-10">
-            <div className="w-full flex sm:items-center font-descFont sm:flex-row flex-col group cursor-pointer">
+            <div className="w-full flex sm:items-center font-descFont sm:flex-row flex-col">
                 <div className="sm:w-1/3 sm:mr-5 overflow-hidden mb-4 sm:mb-0 shadow-[0_3px_8px_0_rgba(0,0,0,0.24)]">
-                    <img src={imgSrc} alt="" className="w-full h-48 object-cover group-hover:scale-110 transition-all duration-200 rounded-sm"/>
+                    <img src={imgSrc} alt="" className="w-full h-48 object-cover hover:scale-110 transition-all duration-200"/>
                 </div>
                 <div className="sm:w-2/3 mb-6">
                     <div className="font-light text-sm opacity-60">{date}</div>
-                    <a href="#" className="font-medium text-gray-700 text-xl tracking-wide group-hover:text-rally-primary transition-all duration-100">{title}</a>
-                    <p className="font-light text-black">{paragraph}</p>
+                    <a href="#" className="font-medium text-[#f0f2fe] cursor-pointer text-xl tracking-wide">{title}</a>
+                    <p className="font-light opacity-60">{paragraph}</p>
                 </div>
             </div>
         </div>
     );
 };
 const NewsContainer = () => {
-    const newsData = [
-        {
-            date: '05-08-2024 15:05',
-            title: 'Informācija rallija ekipāžām',
-            paragraph: 'Šodien, 23. jūlijā, ir pēdējā diena, lai pieteiktos rallijam Cēsis 2024 par zemāku dalības maksu, līdz 26. jūlijam tā tiks paaugstināta.',
-            imgSrc: '/images/newsImages/img1.png',
-        },
-        {
-            date: '04-08-2024 14:00',
-            title: 'Rallija sacensības tuvojas',
-            paragraph: 'Rallija sacensības tuvojas, un dalībniekiem ir jābūt gataviem izaicinājumam. Līdzjutēji tiek aicināti piedalīties.',
-            imgSrc: '/images/newsImages/img2.png',
-        },
-        {
-            date: '03-08-2024 13:30',
-            title: 'Svarīga informācija dalībniekiem',
-            paragraph: 'Dalībniekiem jāņem vērā jaunākie noteikumi un izmaiņas, kas ir publicētas oficiālajā mājaslapā.',
-            imgSrc: '/images/newsImages/img3.png',
-        },
-    ];
+    const { lng, year, rallyName } = useParams();
+    const { t } = useTranslation();
+    const url = `/news/${year}/${rallyName}`;
+    const STORAGE_URL = process.env.REACT_APP_STORAGE_URL;
+
+    const { data: newsData, loading, error } = useFetchData(url);
+
+    useLanguage(lng);
     return (
-        <section className="w-full min-h-20 bg-white sm:p-14 p-10 flex justify-center">
+        <section className="w-full min-h-20 bg-rally-primary sm:p-14 p-10 flex justify-center">
             <div className="lg:w-[1024px]">
-                <TitleWithLine title="Jaunumi" />
-                {newsData.length > 0 ? (
-                    newsData.map((news, index) => (
-                        <NewsItem
-                            key={index}
-                            date={news.date}
-                            title={news.title}
-                            paragraph={news.paragraph}
-                            imgSrc={news.imgSrc}
-                        />
-                    ))
-                ) : (
-                    <div className="mt-10 text-gray-700 text-center">
-                        Pašlaik nav jaunu ziņu.
-                    </div>
+                <div className="flex items-center">
+                    <div className="font-containerHeading font-bold text-white text-4xl mr-4">{t('rally-menu-bar.news')}</div>
+                    <div className="flex-1 h-0.5 bg-white"></div>
+                </div>
+                {loading && <Loader />}
+                {!loading && error && <div>Error loading data: {error.message}</div>}
+
+                {!loading && !error && (
+                    newsData.length > 0 ? (
+                        newsData.map((news, index) => (
+                            <NewsItem
+                                key={index}
+                                date={news.pub_date_time}
+                                title={news.title}
+                                paragraph={news.paragraph}
+                                imgSrc={STORAGE_URL+news.img_src}
+                            />
+                        ))
+                    ) : (
+                        <div className="mt-10 text-white text-center">
+                            Pašlaik nav jaunu ziņu.
+                        </div>
+                    )
                 )}
             </div>
         </section>
