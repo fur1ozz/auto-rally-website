0f2921d297aa702e84086f61e519b7d093ae345c
fur1ozz <tomassveveris@gmail.com>
2025-04-07 23:22:51 +0300

    1.21 Update sponsors container with data from api endpoint

--- src/components/commonUI/Sponsors.jsx
+++ src/components/commonUI/Sponsors.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import TitleWithLine from "../elements/titleWithLine";
+import {useParams} from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData";
+import Loader from "../elements/loaders/Loader";
 
 const SponsorsItem = ({ name, image, type, url }) => {
     return (
@@ -16,78 +19,39 @@ const SponsorsItem = ({ name, image, type, url }) => {
 };
 
 const Sponsors = () => {
-    const fakeSponsors = [
-        {
-            name: "Pirelli",
-            image: "https://www.autorally.lv/files/r235/sponsors/pirelli.png?v=2",
-            type: "Riepu sponsors",
-            url: "https://www.pirelli.com/",
-        },
-        {
-            name: "Neste",
-            image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Neste_logo.png",
-            type: "Galvenais sponsors",
-            url: "https://www.neste.lv/",
-        },
-        {
-            name: "LMT",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA-hepaYIZnvw6ljEc8qAW9Rq9ohR9anSNiA&s",
-            type: "Atbalstītājs",
-            url: "https://www.lmt.lv/",
-        },
-        {
-            name: "Bosch",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Bosch-logo.svg/2560px-Bosch-logo.svg.png",
-            type: "Sadarbībā ar",
-            url: "https://www.bosch.com/",
-        },
-        {
-            name: "TV3 Group",
-            image: "https://logosandtypes.com/wp-content/uploads/2021/02/TV3-Play.png",
-            type: "Informatīvais partneris",
-            url: "https://tv3.lv/",
-        },
-        {
-            name: "Shell Helix",
-            image: "https://vectorise.net/logo/wp-content/uploads/2011/01/Shelll-Helix3.jpg",
-            type: "Atbalstītājs",
-            url: "https://www.shell.com/motorist/oils-lubricants/helix.html",
-        },
-        {
-            name: "Hankook",
-            image: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Hankook_logo.png",
-            type: "Riepu sponsors",
-            url: "https://www.hankooktire.com/global/en/home.html",
-        },
-        {
-            name: "Bosch Car Service",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Bosch_Car_Service_Logo_1999.svg/1200px-Bosch_Car_Service_Logo_1999.svg.png",
-            type: "Sadarbībā ar",
-            url: "https://www.boschcarservice.com/",
-        },
-        {
-            name: "Castrol",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoiLtVNk4eQFbA8LKgJ6Jee6XHwY09XCotzA&s",
-            type: "Atbalstītājs",
-            url: "https://www.castrol.com/",
-        },
-    ];
+    const { year, rallyName } = useParams();
+    const url = `/sponsors/${year}/${rallyName}/`;
+
+    const STORAGE_URL = process.env.REACT_APP_STORAGE_URL;
+
+    const { data: sponsorData, loading, error } = useFetchData(url);
 
     return (
         <section className="w-full bg-white sm:px-14 px-10 pt-10 flex justify-center">
             <div className="lg:w-[1024px] w-full">
                 <TitleWithLine title="Mūsu partneri" />
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 my-10 text-[#4e4e4e] font-chakra">
-                    {fakeSponsors.map((sponsor, index) => (
-                        <SponsorsItem
-                            key={index}
-                            name={sponsor.name}
-                            image={sponsor.image}
-                            type={sponsor.type}
-                            url={sponsor.url}
-                        />
-                    ))}
-                </div>
+                {loading && <Loader />}
+                {!loading && error && <div>Error loading data: {error.message}</div>}
+
+                {!loading && !error && (
+                    sponsorData.length > 0 ? (
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 my-10 text-[#4e4e4e] font-chakra">
+                            {sponsorData.map((sponsor, index) => (
+                                <SponsorsItem
+                                    key={index}
+                                    name={sponsor.name}
+                                    image={STORAGE_URL + sponsor.image_url}
+                                    type={sponsor.type}
+                                    url={sponsor.url}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="mt-10 text-[#4e4e4e] text-center">
+                            Pašlaik nav publicētu sponsoru.
+                        </div>
+                    )
+                )}
             </div>
         </section>
     );
